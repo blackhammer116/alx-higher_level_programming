8e31d73c8fc62c9d8f00ee27b0fdde0c9c15617f
Nebiyu <nebiyuhiyabu@gmail.com>
2022-11-29 22:20:12 -0800

    changing letters to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        if ord(i) in range(ord('a'), ord('z') + 1):
+            i = chr(ord(i) - 32)
+        print("{}".format(i), end="")
