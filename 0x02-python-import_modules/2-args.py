222e1917df34628a933db08cf3f4c3718d8677ef
Nebiyu <nebiyuhiyabu@gmail.com>
2022-12-01 08:40:51 -0800

    printing command line arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,9 +4,6 @@ if __name__ == "__main__":
     argc = len(argv) - 1
     if argc == 0:
         print("0 arguments.")
-    elif argc == 1:
-        print("{}: arguments:".format(argc))
-        print("1: {}".format(argv[1]))
     else:
         print("{} arguments:".format(argc))
         i = 1
