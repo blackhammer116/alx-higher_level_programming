afff2b994202932fecf8f19f5db399b77d5d6f93
Nebiyu <nebiyuhiyabu@gmail.com>
2022-12-01 08:56:06 -0800

    adding numbers in command line argument

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    argc = len(argv) - 1
     n = 0
-    if argc == 0:
-        print("0")
-    else:
-        for args in argv[1:]:
-            n += int(args)
-        print(n)
+    for args in argv[1:]:
+        n += int(args)
+    print(n)
