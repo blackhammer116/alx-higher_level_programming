94423ee4a833e1b801e68893040971a0eb3875c6
Nebiyu <nebiyuhiyabu@gmail.com>
2022-12-04 06:37:47 -0800

    retrieving an element from list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -5,7 +5,7 @@ def element_at(my_list, idx):
         return None
     for i in my_list:
         count += 1
-    if idx > count:
+    if idx >= count:
         return None
     else:
         return my_list[idx]
