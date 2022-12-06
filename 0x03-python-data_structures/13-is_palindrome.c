289b4865c9b7266c0d2abad60cff983912e76a8d
Nebiyu <nebiyuhiyabu@gmail.com>
2022-12-05 23:10:42 -0800

    checking if a list is a palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,28 +1,5 @@
 #include "lists.h"
 /**
- * rev_list - function that reverses a linked list
- * @head: head pointer to a linked list
- */
-void rev_list(listint_t **head)
-{
-	listint_t *current, *prev = NULL, *nxt;
-	current = *head;
-	
-	while(current)
-	{
-		nxt = current->next;
-		current->next = prev;
-		prev = current;
-		current = nxt;
-	}
-	current = prev;
-}
-
-
-
-
-
-/**
  * is_palindrome - checks if a singly linked list is palindrome
  * @head: pointer to the head of the list
  * Return: 1 if its palindrome, else 0
