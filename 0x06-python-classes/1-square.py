695a13ff6aa4c097d720dc8219e90a6df64ca67f
Nebiyu <nebiyuhiyabu@gmail.com>
2023-01-25 03:25:06 -0800

    creating a private instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""creating a private instace attribute"""
+
+class Square():
+    """class Square with
+
+    Attributes:
+        __size: private instace attribute
+
+    """
+    __size
