a56a501541e2eb414676235fea15e459c815664d
Nebiyu <nebiyuhiyabu@gmail.com>
2023-01-25 04:46:39 -0800

    Modified version of class Square, that returns the area of the square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -12,8 +12,10 @@ class Square():
                 raise ValueError('size must be >= 0')
             if type(size) is not int:
                 raise TypeError('size must be an integer')
-        except(TypeError):
+        except (TypeError):
             raise TypeError('size must be an integer')
+
     def area(self):
         if self.__size:
-            return self.__size ** 2 
+            return self.__size ** 2
+        return
