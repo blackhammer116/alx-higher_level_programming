0fd164a633a9ee7571b98b07ed205a035f9f08f7
Nebiyu <nebiyuhiyabu@gmail.com>
2023-01-25 03:11:00 -0800

    creating an empty class

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""Defining an empty class"""
+class Square():
+    """Empty square class"""
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+This repo contains introduction to OOP with python projects
