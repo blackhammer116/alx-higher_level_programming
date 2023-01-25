a7923a4e907c9a3bc4c495e909ad2a216506b32d
Nebiyu <nebiyuhiyabu@gmail.com>
2023-01-25 03:47:30 -0800

    creating a class that returns the area

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""creating a private instace attribute"""
+
+
+class Square():
+    """class Square with a private instance attribute and checks the validity"""
+    def __init__(self, size=0):
+        try:
+            self.__size = size
+            if __size < 0:
+                raise ValueError('size must be >= 0')
+        except (TypeError):
+
+        def area(self):
+            return __size * __size
