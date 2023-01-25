56ad46882ac6f551551d512b35eccdd4e95bbb14
Nebiyu <nebiyuhiyabu@gmail.com>
2023-01-25 04:25:06 -0800

    Updating the square class so that it can return the area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""creating a private instace attribute"""
+
+
+class Square():
+    """class Square with a private instance attribute
+        and checks the validity"""
+    def __init__(self, size=0):
+        try:
+            self.__size = size
+            if size < 0:
+                raise ValueError('size must be >= 0')
+            if type(size) is not int:
+                raise TypeError('size must be an integer')
+        except(TypeError):
+            raise TypeError('size must be an integer')
+    def area(self):
+        return size * size
