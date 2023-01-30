ece2f6933eafee95a670ce3211a8508366447667
Nebiyu <nebiyuhiyabu@gmail.com>
2023-01-30 03:42:25 -0800

    adding area and perimeter calculating methods

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""  defining a class rectangle and adding two methods
+    area and perimeter
+"""
+
+
+class Rectangle:
+    """ class rectangle """
+    def __init__(self, width=0, height=0):
+        """ initalizing the parameters
+            Args:
+                width (int): width of the class
+                height (int): height of the class
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ getter for width """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ setter for width
+            Args:
+                value (int): value to be setted
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """ getter for height """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ setter for height
+            Args:
+                value (int): value to be setted
+        """
+
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        return self.width * self.height
+    def perimeter(self):
+        if self.width == 0 or self.width == 0:
+            return 0
+        else:
+            return 2 * (self.width + self.height)
