7e5b80c5e30fde653b7656356eb31d18e3812d27
Nebiyu <nebiyuhiyabu@gmail.com>
2023-01-30 03:26:12 -0800

    defining the rectangle class

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,57 +1,51 @@
 #!/usr/bin/python3
 """  defining a class rectangle with width and height """
 
-
 class Rectangle:
     """ class rectangle """
     def __init__(self, width=0, height=0):
         """ initalizing the parameters
-
             Args:
                 width (int): width of the class
                 height (int): height of the class
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
+    @property
     def width(self):
         """ getter for width """
         return self.__width
 
+    @width.setter
     def width(self, value):
         """ setter for width
             Args:
                 value (int): value to be setted
         """
-        try:
-            if type(value) is not int:
-                raise TypeError("width must be an integer")
-            elif value < 0:
-                raise ValueError("width must be >= 0")
-            else:
-                self.__width = value
-        except (TypeError):
+        if type(value) is not int:
             raise TypeError("width must be an integer")
-        except (ValueError):
+        elif value < 0:
             raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
 
+
+    @property
     def height(self):
         """ getter for height """
         return self.__height
 
+    @height.setter
     def height(self, value):
         """ setter for height
             Args:
                 value (int): value to be setted
         """
-        try:
-            if type(value) is not int:
-                raise TypeError("height must be an integer")
-            elif value < 0:
-                raise ValueError("height must be >= 0")
-            else:
-                self.__height = value
-        except (TypeError):
-            raise TypeError("width must be an integer")
-        except (ValueError):
-            raise ValueError("width must be >= 0")
+
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
