fef8ace293158f42cc54ec32555d226994ff28db
Nebiyu <nebiyuhiyabu@gmail.com>
2023-02-01 03:42:55 -0800

    customizing printable symbole

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -66,13 +66,13 @@ class Rectangle:
             return 2 * (self.width + self.height)
 
     def __str__(self):
-        r = Rectangle.print_symbol
+        r = ""
 
         if self.width == 0 or self.height == 0:
             return r
         for j in range(self.height):
             for i in range(self.width):
-                r += Rectangle.print_symbol
+                r += str(self.print_symbol)
 
             if j != self.height - 1:
                 r += "\n"
