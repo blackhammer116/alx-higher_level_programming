48adc741613589e59e6a6bcc74ad532ad7b70981
Nebiyu <nebiyuhiyabu@gmail.com>
2023-02-01 03:56:27 -0800

    comparing two rectangle objects

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -88,7 +88,7 @@ class Rectangle:
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         if type(rect_1) is not Rectangle:
-            rasie TypeError("rect_1 must be an instance of Rectangle")
+            raise TypeError("rect_1 must be an instance of Rectangle")
         if type(rect_2) is not Rectangle:
             raise TypeError("rect_2 must be an instance of Rectangle")
         if rect_1.area() > rect_2.area():
