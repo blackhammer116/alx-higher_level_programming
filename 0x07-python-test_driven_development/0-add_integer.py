558a9f3aae90d7f58fc73be9c3e293e212510411
Nebiyu <nebiyuhiyabu@gmail.com>
2023-03-02 01:36:15 -0800

    first test

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-import math
-""" math module """
-
-
 def add_integer(a, b=98):
     """ Addition function """
 
     if type(a) is float:
-        a = math.floor(a)
+        a = int(a)
     if type(b) is float:
-        b = math.floor(b)
+        b = int(b)
     if type(a) is not int:
         raise TypeError("a must be an integer")
     if type(b) is not int:
