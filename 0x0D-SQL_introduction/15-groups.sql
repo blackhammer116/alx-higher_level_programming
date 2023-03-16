60125b0fa12c11eab04cf0a547b907ce4c4e7968
Nebiyu <nebiyuhiyabu@gmail.com>
2023-03-16 09:13:22 -0700

    grouping rows

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- selecting rows with identical values
 SELECT score,COUNT(score) AS "number" FROM second_table GROUP BY score
-HAVING COUNT(score) > 1 ORDER BY SCORE DESC;
+HAVING COUNT(score) > 1 ORDER BY number DESC;
