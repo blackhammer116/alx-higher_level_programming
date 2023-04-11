565b3b1d8c2f09785049c5fced0ffcf7978b513c
Nebiyu <nebiyuhiyabu@gmail.com>
2023-04-11 17:07:31 +0000

    Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const process = require('process');
+const args = process.argv;
+
+if (args.length === 3) { 
+  console.log('Argument found');
+} else if (args.length > 3) {
+  console.log('Arguments found');
+} else {
+  console.log('No argument');
+}
