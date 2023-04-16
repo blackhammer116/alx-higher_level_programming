cf4f8cd5048d3e33b9fa19c9b90b0c2401292f98
Nebiyu <nebiyuhiyabu@gmail.com>
2023-04-16 06:04:18 -0700

    small fix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-""" imports mysql database """
-""" imports system in order to take in command line argument """
 import sys
 import MySQLdb
+"""
+    imports mysql database
+    imports system in order to take in command line argument
+    """
 
 
 if __name__ == "__main__":
     """ listes all of the states in ascending order """
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(
+            host="localhost", port=3306, user=sys.argv[1],
+            passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     query_fetch = cur.fetchall()
