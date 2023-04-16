70def5e5d6b5649da8edd7ada74a6a46fa4b61eb
Nebiyu <nebiyuhiyabu@gmail.com>
2023-04-16 07:02:50 -0700

    filtering through the names

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+    Script that selects states that have their names starting
+    with "N"
+
+    using two modules MySQLdb and sys for database connection
+    and for getting command line arguments
+"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    """
+    Getting command line argument and executing the query
+    """
+    db = MySQLdb.connect(
+            host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    query_fetch = cur.fetchall()
+    for row in query_fetch:
+        print(row)
+    cur.close()
+    db.close()
