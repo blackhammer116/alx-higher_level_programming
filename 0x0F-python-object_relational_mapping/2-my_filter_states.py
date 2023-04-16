72d297faa93a4fad9931e8853771d37e336d6c0a
Nebiyu <nebiyuhiyabu@gmail.com>
2023-04-16 07:30:48 -0700

    filtering via command line argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+    Script that selects states that have their names starting
+    with "N"
+
+    using two modules MySQLdb and sys for database connection
+    and for getting command line arguments
+"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    """
+    Getting command line argument and executing the query
+    """
+    N = sys.argv[4]
+    db = MySQLdb.connect(
+            host="localhost", port=3306,
+            user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute(
+            "SELECT * FROM states WHERE name = %s", (sys.argv[4],))
+    query_fetch = cur.fetchall()
+    for row in query_fetch:
+        print(row)
+    cur.close()
+    db.close()
