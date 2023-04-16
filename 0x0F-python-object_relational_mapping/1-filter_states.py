1eedc1249ddcea75f5bf23e5cb5157248e6260bf
Nebiyu <nebiyuhiyabu@gmail.com>
2023-04-16 07:07:51 -0700

    bug fix

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -15,9 +15,11 @@ if __name__ == "__main__":
     Getting command line argument and executing the query
     """
     db = MySQLdb.connect(
-            host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+            host="localhost", port=3306,
+            user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE UPPER('N%') ORDER BY id ASC")
+    cur.execute(
+            "SELECT * FROM states WHERE name LIKE UPPER('N%') ORDER BY id ASC")
     query_fetch = cur.fetchall()
     for row in query_fetch:
         print(row)
