a7b4b32cb062bbcfd1d0854352a09f3c6c35e0b7
Nebiyu <nebiyuhiyabu@gmail.com>
2023-04-17 05:56:44 -0700

    model state

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+    Script that creates a class state and intergrated with the table
+
+    Using SQLAlchemy module
+"""
+from sqlalchemy import Integer, Column, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """
+        State class which will be later integrated with states table
+
+        Attributes:
+            __tablename__: name of the table in use
+            id: id of the state
+            name: name of the state
+    """
+
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key = True)
+    name = Column(String(128), nullable = False)
