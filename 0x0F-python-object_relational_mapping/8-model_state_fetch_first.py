4ba1759d9772599df62eae33013b02ca7331536a
Nebiyu <nebiyuhiyabu@gmail.com>
2023-04-18 04:27:49 -0700

    fetching the first state

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+    Script that lists all staets ordered by state.id
+
+    Using MySQLdb, sys and SQLAlchemy
+"""
+import sys
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from model_state import State, Base
+
+if __name__ == "__main__":
+    """
+        Here we create an engine, create an instance of Session and
+        list all states based on our objects not query
+    """
+
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'
+            .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    first = session.query(State).filter(State.id == 1).first()
+    print("{}: {}".format(first.id, first.name))
+    session.close()
