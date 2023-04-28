7f40e107f04a23abe3048598b388d10f5a838f9b
Nebiyu <nebiyuhiyabu@gmail.com>
2023-04-28 10:18:39 -0700

    getting size of body

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes a URL from command line and uses curl to get the size of the body of the response
-curl -s "$1" -w "%{size_download}\n" | tail -n 1
+curl -s "$1" | wc -c
