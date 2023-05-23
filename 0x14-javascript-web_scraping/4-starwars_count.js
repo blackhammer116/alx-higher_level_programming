e56c2f15ef633d75fc29e23ec95fef00be587d72
Nebiyu <nebiyuhiyabu@gmail.com>
2023-05-23 19:28:43 +0000

    counting the films

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const request = require('request');
+const url1 = (process.argv[2].slice(0,-5)) + 'people/18';
+
+request(url1, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log((JSON.parse(body).films).length);
+    //con url3 ;
+    //console.log(url2);
+  }
+});
