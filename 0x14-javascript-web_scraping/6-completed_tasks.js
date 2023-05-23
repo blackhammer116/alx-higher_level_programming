68b01651e91cea1983ffad0756e2d818964da97c
Nebiyu <nebiyuhiyabu@gmail.com>
2023-05-23 21:03:31 +0000

    completed tasks

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -6,30 +6,19 @@ request(url, function (error, response, body) {
   if (error) {
     console.error(error);
   } else {
-      const todo = JSON.parse(body);
-      
-      const completed_task = {};
+    const todo = JSON.parse(body);
 
-      todo.forEach((todo) => {
-        if (todo.completed){
-          if (completed_task[todo.userId]) {
-            completed_task[todo.userId]++;
-          } else {
-              completed_task[todo.userId] = 1;
-            }
+    const completedTask = {};
+
+    todo.forEach((todo) => {
+      if (todo.completed) {
+        if (completedTask[todo.userId]) {
+          completedTask[todo.userId]++;
+        } else {
+          completedTask[todo.userId] = 1;
         }
-      });
-/*
-      Object.keys(completed_task).forEach((userId) => {
-        if (!completed_task[userId - 1]) {
-          console.log('{');
-          console.log(`'${userId}': ${completed_task[userId]},`);
-        } else if (completed_task[userId + 1]) {
-          console.log(`'${userId}': ${completed_task[userId]},`);
-        } else if (!completed_task[userId + 1]){
-          console.log(`'${userId}': ${completed_task[userId]} \}`);
-        }
-        });*/
-    console.log(completed_task);
+      }
+    });
+    console.log(completedTask);
   }
 });
