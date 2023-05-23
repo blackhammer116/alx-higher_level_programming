08cf6106f9954b4d35455b801ff4b4655799e68f
Nebiyu <nebiyuhiyabu@gmail.com>
2023-05-23 16:27:14 +0000

    readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,2 +1,11 @@
-!/usr/bin/node
+#!/usr/bin/node
+const fs = require('fs');
+const url = process.argv[2];
 
+fs.readFile(url, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+});
