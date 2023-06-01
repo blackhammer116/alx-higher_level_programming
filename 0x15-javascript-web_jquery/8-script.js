15b9cd7c9fecfabd73b3e896d1059da47330dffd
Nebiyu <nebiyuhiybu@gmail.com>
2023-06-01 01:36:39 +0000

    star wars titles

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,7 @@
+$.getJSON('https://swapi-api.alx-tools.com/api/films/?format=json', function(data) {
+    const movies = data.results;
+    const list = $('#list_movies');
+    $.each(movies, function(index, movie) {
+        list.append('<li>' + movie.title + '</li>');
+    });
+});
