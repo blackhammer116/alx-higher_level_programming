a79a0620311b4249b3f6a5bc113c01ac3287d03d
Nebiyu <nebiyuhiyabu@gmail.com>
2023-07-04 04:18:35 -0700

    optimized the code

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 #sending a get request to a url
-url=$1
-response=$(curl -s -w "%{http_code}" $url)
-
-status_code=${response: -3}
-body=${response::-3}
-
-if [ $status_code -eq 200 ]; then
-    echo $body
-fi
+curl -s -w "\n%{http_code}" $1 | awk 'END{if($0==200)exit;print $0}'
