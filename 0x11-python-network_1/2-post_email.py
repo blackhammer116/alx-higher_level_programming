2bcb7d073d46cf8a217576ec3ff02aaa5ff11cc1
Nebiyu <nebiyuhiyabu@gmail.com>
2023-07-05 07:40:15 -0700

    bug fixed

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,14 +4,15 @@ urllib.request: module to get response of a url
 urllib.parse: module to parse the content
 argv: module to get command line argument
 """
-import urllib.request, urllib.parse
+import urllib.request
+import urllib.parse
 from sys import argv
 
 if __name__ == "__main__":
     url = argv[1]
     email = argv = argv[2]
 
-    data = urllib.parse.urlencode({'email':email}).encode('utf-8')
+    data = urllib.parse.urlencode({'email' : email}).encode('utf-8')
     req = urllib.request.Request(url, data=data, method='POST')
 
     with urllib.request.urlopen(req) as response:
