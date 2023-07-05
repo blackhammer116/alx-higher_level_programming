12ab166b9ae886fcc185732cfc2bf9c26645a03d
Nebiyu <nebiyuhiyabu@gmail.com>
2023-07-05 09:04:03 -0700

    interview question

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,9 +8,10 @@ import sys
 
 if __name__ == "__main__":
     url = 'https://api.github.com/repos/{}/{}/commits'.format(sys.argv[1],sys.argv[2])
-    response = requests.get(url)
+    params = {'per-page': 10}
+    response = requests.get(url, params=params)
     response_json = response.json()
-    for commit in response_json[:10]:
+    for commit in response_json:
         sha = commit['sha']
         name = commit['commit']['author']['name']
         print(f'{sha}: {name}')
