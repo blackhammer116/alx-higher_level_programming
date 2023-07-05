61d6ec9e3b4abd23dc95258e1a9d6ff770cb3cfd
Nebiyu <nebiyuhiyabu@gmail.com>
2023-07-05 09:22:03 -0700

    interview question

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,7 +7,8 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    url = 'https://api.github.com/repos/{}/{}/commits'.format(sys.argv[2],sys.argv[1])
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(
+            sys.argv[2],sys.argv[1])
     params = {'per-page': 9}
     response = requests.get(url, params=params)
     response_json = response.json()
