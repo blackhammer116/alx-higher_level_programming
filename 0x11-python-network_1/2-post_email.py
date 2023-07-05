205390aa2587675acd8d92d438069b21272b8a44
Nebiyu <nebiyuhiyabu@gmail.com>
2023-07-05 07:41:40 -0700

    bug fixed

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     url = argv[1]
     email = argv = argv[2]
 
-    data = urllib.parse.urlencode({'email' : email}).encode('utf-8')
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
     req = urllib.request.Request(url, data=data, method='POST')
 
     with urllib.request.urlopen(req) as response:
