8a528fefc88e58240ed4b779f8c144b6a100b85f
Nebiyu <nebiyuhiyabu@gmail.com>
2023-07-05 07:46:26 -0700

    bug fixed

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,7 +9,7 @@ import sys
 if __name__ == "__main__":
     try:
         url = "http://0.0.0.0:5000/search_user"
-        letter = sys.argv[2] if len(sys.argv) > 1 else ""
+        letter = sys.argv[2] if len(sys.argv[]) > 1 else ""
         data = {'q':letter}
 
         response = requests.post(url, data=data)
