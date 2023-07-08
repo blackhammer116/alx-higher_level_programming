d04af1d82a2b0d1272e71eb6ef01bd18f973a607
Nebiyu <nebiyuhiybu@gmail.com>
2023-07-08 10:42:06 -0700

    fixed small bug

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -2,16 +2,18 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0){
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
+    }
   }
   print() {
-    for (let i = 0; i < this.height; i++){
-      for (let j = 0; j < this.width; j++){
-        console.log("X" + '');
+    for (i = 0; i < this.height; i++){
+      let row = "";
+      for (j = 0; j < this.width; j++){
+        row += "X";
       }
-      console.log('');
+      console.log(row);
     }
   }
 }
