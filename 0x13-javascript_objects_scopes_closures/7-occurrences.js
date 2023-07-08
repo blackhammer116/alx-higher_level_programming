015eb6596c4c33556b5df1ad3fa541221036f6a1
Nebiyu <nebiyuhiybu@gmail.com>
2023-07-08 11:36:48 -0700

    small bugg fixed

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,8 +2,8 @@
 
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
-  for (let i = 0; list[i] !== null; i++) {
-    if (list[i] === searchElement) {
+  for (let i = 0; i < list.lenght; i++) {
+    if (list[i] == searchElement) {
       count++;
     }
   }
