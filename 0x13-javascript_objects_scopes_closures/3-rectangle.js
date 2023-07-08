96e6666410e74543dc29d7869751ca56cbd550f2
Nebiyu <nebiyuhiybu@gmail.com>
2023-07-08 10:23:36 -0700

    corrected output

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -6,7 +6,7 @@ class Rectangle {
       this.width = w;
       this.height = h;
   }
-  print(){
+  print() {
     for (let i = 0; i < this.height; i++){
       for (let j = 0; j < this.width; j++){
         console.log("X" + '');
